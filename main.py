e43b13fa27a01d809b6e55caad01677cacf7921e
Sankalp Singh <singhsankalp997@gmail.com>
2024-11-29 15:59:23 +0530

    main file added

---
+++ main.py
@@ -0,0 +1,18 @@
+from backtester import Backtester
+from workflow import run_agent
+import pandas as pd
+
+ticker="AAPL"
+start_date="2024-01-01"
+end_date="2024-12-31"
+initial_capital=10000
+
+backtester=Backtester(
+    agent=run_agent,
+    ticker=ticker,
+    start_date=start_date,
+    end_date=end_date,
+    initial_capital=initial_capital
+)
+backtester.run_backtest()
+performance_df = backtester.analyze_performance()
